815f9c5171f0c7d869d12797d16ddf5c4a33905b
Ashraf Ibraheem <114275226+Fresh-Quail@users.noreply.github.com>
2024-10-02 14:19:09 -0400

    Compiled both top cell and top module

--- HW2/P1/ALU.v
+++ HW2/P1/ALU.v
@@ -5,79 +5,53 @@ input CIN, COE;
 output [15:0] C;
 output COUT;
 output VOUT;
-wire cla_cout[3:0];
-wire [15:0] Bop;
 
 //NOTE: Overflow checking is different for signed and unsigned subtraction
+wire cla_cout[3:0];
+wire [15:0] Bop;
 reg [15:0] V, cout;
+
 assign VOUT = V;
 assign COUT = cout;
+assign Bop = CODE[2] ? (CODE[0] ?  16'h0001 : 16'hFFFF) : B ^ CODE[1];
+
+sumGenerator cla_0(.A(A[3:0]), .B(Bop[3:0]), .Cin(CODE[1]), .sum(C[3:0]), .Cout_final(cla_cout[0]) ); 
+sumGenerator cla_1(.A(A[7:4]), .B(Bop[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
+sumGenerator cla_2(.A(A[11:8]), .B(Bop[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
+sumGenerator cla_3(.A(A[15:12]), .B(Bop[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) );
+
 always @(*) begin
     case(CODE[2:0])
         //Done
         // signed addition
         3'b000: begin
-            
-            sumGenerator cla_0(.A(A[3:0]), .B(B[3:0]), .Cin(CIN), .sum(C[3:0]), .Cout_final(cla_cout[0]) );
-            sumGenerator cla_1(.A(A[7:4]), .B(B[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
-            sumGenerator cla_2(.A(A[11:8]), .B(B[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
-            sumGenerator cla_3(.A(A[15:12]), .B(B[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) );
             cout = COE ? 1'bx : cla_cout[3];
             V = (A[15] & B[15] & (~C[15])) | ((~A[15] & ~B[15]) & C[15]);
         end
         //Done
         // unsigned addition
         3'b001: begin
-            
-            sumGenerator cla_0(.A(A[3:0]), .B(B[3:0]), .Cin(CIN), .sum(C[3:0]), .Cout_final(cla_cout[0]) ) ;
-            sumGenerator cla_1(.A(A[7:4]), .B(B[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) ) ;
-            sumGenerator cla_2(.A(A[11:8]), .B(B[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) ) ;
-            sumGenerator cla_3(.A(A[15:12]), .B(B[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) ) ;
             cout = COE ? 1'bx : cla_cout[3];
             V = cout;
         end
         //NOTE: Can probably collapse signed addition and subtraction into one case
         // signed subtraction
         3'b010: begin
-            wire [15:0] Bop = B ^ CODE[0];
-           
-            sumGenerator cla_0(.A(A[3:0]), .B(Bop[3:0]), .Cin(1'b1), .sum(C[3:0]), .Cout_final(cla_cout[0]) ); //Cin set to 1
-            sumGenerator cla_1(.A(A[7:4]), .B(Bop[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
-            sumGenerator cla_2(.A(A[11:8]), .B(Bop[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
-            sumGenerator cla_3(.A(A[15:12]), .B(Bop[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) );
             cout = COE ? 1'bx : cla_cout[3];
             V = (A[15] & B[15] & (~C[15])) | ((~A[15] & ~B[15]) & C[15]);
         end
         // unsigned subtraction
         3'b011: begin
-            wire [15:0] Bop = B ^ CODE[0];
-            
-            sumGenerator cla_0(.A(A[3:0]), .B(Bop[3:0]), .Cin(1'b1), .sum(C[3:0]), .Cout_final(cla_cout[0]) ); //Cin set to 1
-            sumGenerator cla_1(.A(A[7:4]), .B(Bop[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
-            sumGenerator cla_2(.A(A[11:8]), .B(Bop[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
-            sumGenerator cla_3(.A(A[15:12]), .B(Bop[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) ) ;
             cout = COE ? 1'bx : cla_cout[3];
             V = ~cout;
         end
         // signed increment
         3'b100: begin
-            wire [15:0] inc = 4'h0001;
-            
-            sumGenerator cla_0(.A(A[3:0]), .B(inc[3:0]), .Cin(CIN), .sum(C[3:0]), .Cout_final(cla_cout[0]) );
-            sumGenerator cla_1(.A(A[7:4]), .B(inc[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
-            sumGenerator cla_2(.A(A[11:8]), .B(inc[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
-            sumGenerator cla_3(.A(A[15:12]), .B(inc[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) );
             assign cout = COE ? 1'bx : cla_cout[3];
             assign V = (A[15] & B[15] & (~C[15])) | ((~A[15] & ~B[15]) & C[15]);
         end
         //signed decrement
         3'b101: begin
-            wire [15:0] dec = 4'hFFFF;
-           
-            sumGenerator cla_0(.A(A[3:0]), .B(dec[3:0]), .Cin(CIN), .sum(C[3:0]), .Cout_final(cla_cout[0]) );
-            sumGenerator cla_1(.A(A[7:4]), .B(dec[7:4]), .Cin(cla_cout[0]), .sum(C[7:4]), .Cout_final(cla_cout[1]) );
-            sumGenerator cla_2(.A(A[11:8]), .B(dec[11:8]), .Cin(cla_cout[1]), .sum(C[11:8]), .Cout_final(cla_cout[2]) );
-            sumGenerator cla_3(.A(A[15:12]), .B(dec[15:12]), .Cin(cla_cout[2]), .sum(C[15:12]), .Cout_final(cla_cout[3]) );
             cout = COE ? 1'bx : cla_cout[3];
             V = (A[15] & B[15] & (~C[15])) | ((~A[15] & ~B[15]) & C[15]);
         end
--- HW2/P1/FSM.v
+++ HW2/P1/FSM.v
@@ -3,67 +3,74 @@ input [15:0] A, B;
 input [4:0] alu_code;
 output [15:0] C;
 output overflow;
+wire [15:0] Sum_out;
 
-// always@(*) begin
-case(alu_code[4:3])
-    2'b00: begin
-            ALU alu(A, B, alu_code[2:0], 1'b0, 1'b0, C, VOUT, COUT); //Cin set to 0 - COE set to 1 (inactive High)
-    end
 
-    2'b01: begin
-        case(alu_code[2:0])
-            3'b000: assign C = A & B;
-            3'b001: assign C = A | B;
-            3'b010: assign C = A ^ B;
-            3'b100: assign C = ~A;
-        endcase
-    end
+reg [15:0] C;
+reg overflow;
+wire [2:0] code = &alu_code[4:3] ? 3'b010 : alu_code[2:0];
 
-    2'b10: begin
-        case(alu_code[2:0])
-            3'b000: assign C = A << B[3:0];
-            3'b001: assign C = A >> B[3:0];
-            3'b010: assign C = A <<< B[3:0];
-            3'b011: assign C = A >>> B[3:0];
-        endcase
-    end
+ALU alu(A, B, code, 1'b0, 1'b0, Sum_out, VOUT, COUT); //Cin set to 0 - COE set to 1 (inactive High)
 
-    2'b11: begin
-        case(alu_code[2:0])
-            wire compare;
-            wire [15:0] Sum_out;
-            ALU alu(A, B, 3'b010, 1'b0, COE, Sum_out, VOUT, COUT); //Subtract B from A
-            // A <= B
-            3'b000: begin 
-                assign C = A[15] ^ B[15] ? (A[15]) : ((Sum_out[15]) | ~|Sum_out);
-               
-            end
+always @(*) begin
+    case(alu_code[4:3])
+        2'b00: begin
+            C = Sum_out;
+            overflow = VOUT;
+        end
 
-            3'b001: // A < B
-                // If MSB of A & B are different A[15] ^ B[15] = 1
-                // And B is negative (1), then A > B and A = 1 = B 
-                assign C = A[15] ^ B[15] ? (A[15]) : (Sum_out[15]);
+        2'b01: begin
+            case(alu_code[2:0])
+                3'b000: C = A & B;
+                3'b001: C = A | B;
+                3'b010: C = A ^ B;
+                3'b100: C = ~A;
+            endcase
+        end
 
-            // A >= B
-            3'b010: begin 
-                // If MSB of A & B are different A[15] ^ B[15] = 1
-                // And B is negative (1), then A > B and A = 1 = B 
-                // ~|Sum_out == A == B
-                assign C = A[15] ^ B[15] ? (B[15]) : ~(Sum_out[15]);
-            end
-            
-            // A > B
-            3'b011: begin
-                // If MSB of A & B are different A[15] ^ B[15] = 1
-                // And B is negative (1), then A > B and A = 1 = B 
-                assign C = A[15] ^ B[15] ? (B[15]) : ~(Sum_out[15]) & |Sum_out;
-            end
+        2'b10: begin
+            case(alu_code[2:0])
+                3'b000: C = A << B[3:0];
+                3'b001: C = A >> B[3:0];
+                3'b010: C = A <<< B[3:0];
+                3'b011: C = A >>> B[3:0];
+            endcase
+        end
 
-            // A = B
-            3'b100: assign C = ~|Sum_out;
-            // A != B
-            3'b101: assign C = |Sum_out;
-        endcase
-    end
-// end
+        2'b11: begin
+            case(alu_code[2:0])
+                // ALU alu(A, B, 3'b010, 1'b0, COE, Sum_out, VOUT, COUT); //Subtract B from A
+                // A <= B
+                3'b000: begin 
+                    C = A[15] ^ B[15] ? (A[15]) : ((Sum_out[15]) | ~|Sum_out);
+                end
+
+                3'b001: // A < B
+                    // If MSB of A & B are different A[15] ^ B[15] = 1
+                    // And B is negative (1), then A > B and A = 1 = B 
+                    C = A[15] ^ B[15] ? (A[15]) : (Sum_out[15]);
+
+                // A >= B
+                3'b010: begin 
+                    // If MSB of A & B are different A[15] ^ B[15] = 1
+                    // And B is negative (1), then A > B and A = 1 = B 
+                    // ~|Sum_out == A == B
+                    C = A[15] ^ B[15] ? (B[15]) : ~(Sum_out[15]);
+                end
+                
+                // A > B
+                3'b011: begin
+                    // If MSB of A & B are different A[15] ^ B[15] = 1
+                    // And B is negative (1), then A > B and A = 1 = B 
+                    C = A[15] ^ B[15] ? (B[15]) : ~(Sum_out[15]) & |Sum_out;
+                end
+
+                // A = B
+                3'b100: C = ~|Sum_out;
+                // A != B
+                3'b101: C = |Sum_out;
+            endcase
+        end
+    endcase
+end
 endmodule
